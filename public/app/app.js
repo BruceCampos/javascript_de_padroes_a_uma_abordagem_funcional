3208fac1a1fe379b968cc940944bf629e5ae6219
Bruce Campos <brucecamposnh@gmail.com>
2026-01-19 10:15:58 -0300

    Adequação da interação do usuario
    Limita o numero de vezes que a ação pode ser executada, e define um tempo mínimo entre as execuções

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,19 @@
-import { log } from "./utils/promise-helpers.js";
 import "./utils/array-helpers.js";
 import { notasService } from "./nota/service.js";
+import {
+  debounceTime,
+  partialize,
+  takeUntil,
+  pipe,
+} from "./utils/operators.js";
 
-document.querySelector("#myButton").onclick = () =>
-  notasService.sumItems("2143").then(console.log).catch(console.log);
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+  notasService.sumItems("2143").then(console.log).catch(console.log)
+);
+
+document.querySelector("#myButton").onclick = action;
